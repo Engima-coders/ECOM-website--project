1b28fff2de47d34044d350ae27ca9407659f77cb
ashmita-123 <karmakarashmita14@gmail.com>
2025-07-16 21:41:23 +0530

    Add files via upload

--- home.php
+++ home.php
@@ -1,7 +1,6 @@
-
 <?php
-require("./header.php");
-require("./carosal.php");
+require("../components/header.php");
+require("../components/carosal.php");
 ?>
 <div class="main-banner">
     <div class="card cart">
@@ -14,78 +13,78 @@ require("./carosal.php");
             <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
     </div>
-    <img src="./img/mb.jpg" alt="Banner" class="img-responsive mt-3" style="width:100%">
+    <img src="/stylish/src/assets/img/mb.jpg" alt="Banner" class="img-responsive mt-3" style="width:100%">
 </div>
 <h3 class="p-3 mb-2 bg-primary text-white text-center text-boald">Populer Products</h3>
 <div class="container d-flex justify-content-space-between flex-wrap gap-4">
     <div class="card" style="width: 18rem;">
-        <img src="./img/card-image1.jpg" class="card-img-top" alt="...">
+        <img src="https://www.jiomart.com/images/product/original/rvgq7zemz7/sancia-women-georgette-ethnic-top-palazzo-ethnic-jacket-set-for-women-girls-ethnic-wear-for-women-indian-dress-for-women-kurta-set-with-dupatta-floral-embroidered-kurta-maroon-m-product-images-rvgq7zemz7-0-202311071449.jpg?im=Resize=(500,630)" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">WOMEN WEAR</h5>
+            <p class="card-text">Elegance personified in every stitch. Find your perfect outfit and shine with confidence!</p>
+            <a href="#" class="btn btn-primary">Click for more!</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
-        <img src="./img/card-image2.jpg" class="card-img-top" alt="...">
+        <img src="https://assets.myntassets.com/w_412,q_60,dpr_2,fl_progressive/assets/images/5394086/2024/3/27/fac7a437-ba0a-425b-8591-1e55480312611711539929437-Hancock-Men-Black-Slim-Fit-Solid-Formal-Shirt-53171153992888-1.jpg" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">MEN WEAR</h5>
+            <p class="card-text">Sharp, sleek, and stylish. Elevate your wardrobe with our latest collection for men.</p>
+            <a href="#" class="btn btn-primary">Click for more!</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
-        <img src="./img/card-image3.jpg" class="card-img-top" alt="...">
+        <img src="https://peppermint.in/cdn/shop/files/SS25-19176-Multi-G-M-1F3_617e04f0-0e66-4951-81ab-cfd9e6a86db6_332x.jpg?v=1742813453" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">KIDS WEAR</h5>
+            <p class="card-text">Playful, colorful, and oh-so-cute. Dress your little ones in the comfiest and trendiest outfits.</p>
+            <a href="#" class="btn btn-primary">Click for more!</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
-        <img src="./img/card-item4.jpg" class="card-img-top" alt="...">
+        <img src="https://cultstore.com/cdn/shop/files/A_dfc7d72f-8a2d-4006-8262-deb1ef2b304d.jpg?v=1746682665&width=5000" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">FOOT WEAR</h5>
+            <p class="card-text">Step into style and comfort. From sneakers to heels, find the perfect fit for your every step.</p>
+            <a href="#" class="btn btn-primary">Click for more!</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
-        <img src="./img/card-image5.jpg" class="card-img-top" alt="...">
+        <img src="https://images-static.nykaa.com/media/catalog/product/4/5/459410fn73395_1.jpg?tr=w-500" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">ORNAMENTS</h5>
+            <p class="card-text">Adorn yourself with elegance and sophistication. Our ornaments add a touch of glamour to any outfit.</p>
+            <a href="#" class="btn btn-primary">Click for more!</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
-        <img src="./img/card-image6.jpg" class="card-img-top" alt="...">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMhn8LbSHzkcnBhfKg8HKIs8ysR5PWdFApKQ&s" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">WATCH</h5>
+            <p class="card-text">Time is precious, and so is style. Accessorize with our elegant and precise timepieces.</p>
+            <a href="#" class="btn btn-primary">Click for more!</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
-        <img src="./img/card-item7.jpg" class="card-img-top" alt="...">
+        <img src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRKi8D9XOc9zX9SyojLUZAj79W_TI3fhMOEHp4AC2npSiDnMjv8PrBeceEUmbGbvIjZldkibD9l3zFDFtIvwBtiJKbBBhYPoY4LNEGTVG6R" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">TOYS</h5>
+            <p class="card-text">Imagination knows no bounds. Spark creativity and joy with our wide range of fun and educational toys.</p>
+            <a href="#" class="btn btn-primary">Click for more!</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
-        <img src="./img/card-item8.jpg" class="card-img-top" alt="...">
+        <img src="https://rukminim2.flixcart.com/image/704/844/kq5iykw0/makeup-kit/3/k/u/beauty-new-99-beauty-cosmetics-original-imag486zybawksgb.jpeg?q=90&crop=false" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">COSMETICS</h5>
+            <p class="card-text">Unleash your inner beauty. Discover our range of cosmetics that enhance your natural glow and confidence.</p>
+            <a href="#" class="btn btn-primary">Click for more!</a>
         </div>
     </div>
 </div>
 
-<img src="./img/add.jpg" class="img-responsive" style="width:100%">
+<img src="/stylish/src/assets/img/add.jpg" class="img-responsive" style="width:100%">
 
 <?php
-require("./footer.php");
+require("../components/footer.php");
 ?>
