c0651b90efc9789ba42bf071548cf30332482170
ashmita-123 <karmakarashmita14@gmail.com>
2025-07-27 14:58:28 +0530

    Update Products.php

--- src/pages/Products.php
+++ src/pages/Products.php
@@ -1,150 +1,192 @@
 <?php
 require("../components/header.php");
 ?>
-<!--Women section-->
+<!-- Women section -->
 <h3 class="p-3 mb-2 bg-primary text-white text-center text-boald">WOMEN  WEAR</h3>
 <div class="container d-flex justify-content-space-between flex-wrap gap-4">
     <div class="card" style="width: 18rem;">
         <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSQci9bf91XivO2uux6hw-AUoa4aVcEqHiD0csV7vQogS3UmBG1D0UWH0UWKiKF-Azor3MgpWvbN7wa0KTHN33JqlckSmENfe8ADIWQdRU" class="card-img-top" alt="a long frok image">
         <div class="card-body">
             <h5 class="card-title">WESTERN WEAR</h5>
+            <p class="mb-2">₹294</p>
             <p class="card-text">The fashion that will never let you down.</p>
-            <a href="/stylish/src/product_details/p1.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p1.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="https://befashionate.in/cdn/shop/products/tfh-sandalwood-09-sw-406-A_97512530-a847-4bc9-bf21-ea6fae4a8f36.jpg?v=1688054668" class="card-img-top" alt="Saree">
         <div class="card-body">
             <h5 class="card-title">ETHENIC WEAR </h5>
+            <p class="mb-2">₹499</p>
             <p class="card-text">Wrapped in tradition, draped in beauty</p>
-            <a href="/stylish/src/product_details/p2.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p2.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="https://images-static.nykaa.com/media/catalog/product/4/5/459410fn73395_1.jpg?tr=w-500" class="card-img-top" alt="JEWELLARY IMAGE" height="375">
         <div class="card-body">
             <h5 class="card-title">ORNAMENTS</h5>
+            <p class="mb-2">₹239</p>
             <p class="card-text">Jewellery is more than adornment; it's a statement.</p>
-            <a href="/stylish/src/product_details/p3.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p3.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="https://www.algenhealthcare.co/wp-content/uploads/2024/09/Top-15-Cosmetic-Manufacturers-In-Gujarat.jpg" class="card-img-top" alt=".cosmetics" height="375">
         <div class="card-body">
             <h5 class="card-title">COSMETICS</h5>
+            <p class="mb-2">₹499</p>
             <p class="card-text">Never ask a girl with winged eyeliner why she’s late.</p>
-            <a href="/stylish/src/product_details/p4.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p4.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
 </div>
-<!--MEN WEAR-->
+<!-- MEN WEAR -->
 <h3 class="p-3 mb-2 bg-primary text-white text-center text-boald">MEN  WEAR</h3>
 <div class="container d-flex justify-content-space-between flex-wrap gap-4">
     <div class="card" style="width: 18rem;">
         <img src="https://assets.myntassets.com/w_412,q_60,dpr_2,fl_progressive/assets/images/5394086/2024/3/27/fac7a437-ba0a-425b-8591-1e55480312611711539929437-Hancock-Men-Black-Slim-Fit-Solid-Formal-Shirt-53171153992888-1.jpg" class="card-img-top" alt="a shirt image"height="375">
         <div class="card-body">
             <h5 class="card-title">SHIRTS</h5>
+            <p class="mb-2">₹294</p>
             <p class="card-text">You don’t find a style. A style finds you.</p>
-            <a href="/stylish/src/product_details/p5.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p5.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="https://assets.ajio.com/medias/sys_master/root/20231214/wOQq/657ad716afa4cf41f5cff1ec/-473Wx593H-464104985-maroon-MODEL.jpg" class="card-img-top" alt="A maroon ethenic dress"height="375">
         <div class="card-body">
             <h5 class="card-title">T-SHIRTS </h5>
+            <p class="mb-2">₹294</p>
             <p class="card-text">Simplicity is the highest elegance.</p>
-            <a href="/stylish/src/product_details/p6.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p6.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMhn8LbSHzkcnBhfKg8HKIs8ysR5PWdFApKQ&s" class="card-img-top" alt="JEWELLARY IMAGE"height="375">
         <div class="card-body">
             <h5 class="card-title">WATCHES</h5>
+            <p class="mb-2">₹239</p>
             <p class="card-text">A watch is more than an accessory—it's a signature.</p>
-            <a href="/stylish/src/product_details/p7.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p7.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="https://imagescdn.louisphilippe.com/img/app/product/3/39683398-15124912.jpg?auto=format&w=390" class="card-img-top" alt="..."height="375">
         <div class="card-body">
             <h5 class="card-title">SUITS</h5>
+            <p class="mb-2">₹294</p>
             <p class="card-text">Fashions fade, style is eternal.</p>
-            <a href="/stylish/src/product_details/p8.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p8.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
 </div>
-<!--KIDS WEAR-->
+<!-- KIDS WEAR -->
 <h3 class="p-3 mb-2 bg-primary text-white text-center text-boald">KIDS  WEAR</h3>
 <div class="container d-flex justify-content-space-between flex-wrap gap-4">
     <div class="card" style="width: 18rem;">
         <img src="https://manyavar.scene7.com/is/image/manyavar/CDES1125-303+(4)_08-05-2025-13-04:283x395?&dpr=on,2" class="card-img-top" alt="a boy"height="375">
         <div class="card-body">
             <h5 class="card-title">BOYS WEAR</h5>
+            <p class="mb-2">₹305</p>
             <p class="card-text">Style that rocks, confidence that soars.</p>
-            <a href="/stylish/src/product_details/p9.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p9.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="https://peppermint.in/cdn/shop/files/SS25-19176-Multi-G-M-1F3_617e04f0-0e66-4951-81ab-cfd9e6a86db6_332x.jpg?v=1742813453" class="card-img-top" alt="A maroon ethenic dress"height="375">
         <div class="card-body">
             <h5 class="card-title">GIRLS WEAR</h5>
+            <p class="mb-2">₹305</p>
             <p class="card-text">Little princesses in the making, dressed in delight.</p>
-            <a href="/stylish/src/product_details/p10.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p10.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="https://static.vecteezy.com/system/resources/thumbnails/028/535/150/small/a-myriad-of-vibrant-toys-carefully-curated-and-proudly-displayed-generative-ai-photo.jpg" class="card-img-top" alt="JEWELLARY IMAGE" height="375">
         <div class="card-body">
             <h5 class="card-title">TOYS</h5>
+            <p class="mb-2">₹299</p>
             <p class="card-text">Playful smiles, happy memories.</p>
-            <a href="/stylish/src/product_details/p11.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p11.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="https://images.meesho.com/images/products/555302694/wmr3e_512.avif?width=512" class="card-img-top" alt="..."height="375">
         <div class="card-body">
             <h5 class="card-title">SCHOOL KITS</h5>
+            <p class="mb-2">₹399</p>
             <p class="card-text">Learn, grow, and succeed in style</p>
-            <a href="/stylish/src/product_details/p12.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p12.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
 </div>
-<!--Shoes section-->
+<!-- Shoes section -->
 <h3 class="p-3 mb-2 bg-primary text-white text-center text-boald">FOOT  WEAR</h3>
 <div class="container d-flex justify-content-space-between flex-wrap gap-4">
     <div class="card" style="width: 18rem;">
         <img src="https://cultstore.com/cdn/shop/files/A_dfc7d72f-8a2d-4006-8262-deb1ef2b304d.jpg?v=1746682665&width=5000" class="card-img-top" alt="a long frok image">
         <div class="card-body">
             <h5 class="card-title"> RUNNING SHOES</h5>
+            <p class="mb-2">₹795</p>
             <p class="card-text">Run wild, run free.</p>
-            <a href="/stylish/src/product_details/p13.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p13.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="https://m.media-amazon.com/images/I/51QbZMq2IcL._SY695_.jpg" class="card-img-top" alt="A maroon ethenic dress">
         <div class="card-body">
             <h5 class="card-title">WINTER BOOTS </h5>
+            <p class="mb-2">₹599</p>
             <p class="card-text">Step into winter with warmth and style.</p>
-            <a href="/stylish/src/product_details/p14.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p14.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="https://m.media-amazon.com/images/I/51hUvRQ9UlL._SY695_.jpg" class="card-img-top" alt="JEWELLARY IMAGE" height="375">
         <div class="card-body">
             <h5 class="card-title">KIDS SHOES</h5>
+            <p class="mb-2">₹265</p>
             <p class="card-text">Tiny toes, happy moves.</p>
-            <a href="/stylish/src/product_details/p15.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p15.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="https://m.media-amazon.com/images/I/718wgbt1Z0L._SX695_.jpg" class="card-img-top" alt=".cosmetics" height="375">
         <div class="card-body">
             <h5 class="card-title">CROCS</h5>
+            <p class="mb-2">₹265</p>
             <p class="card-text">Comfortable, casual, and carefree.</p>
-            <a href="/stylish/src/product_details/p16.php" class="btn btn-primary">CLICK HERE</a>
+            <a href="/stylish/src/product_details/p16.php" class="btn btn-primary">DETAILS</a>
         </div>
     </div>
 </div>
 <?php
 require("../components/footer.php");
 ?>
+<!-- 
+<?php
+require("../components/header.php");
+include("../../dbcom/conn.php");
+?> -->
+
+<!-- <h3 class="p-3 mb-2 bg-primary text-white text-center text-boald">ALL PRODUCTS</h3>
+<div class="container d-flex justify-content-space-between flex-wrap gap-4"> -->
+    <!-- <?php
+    $result = mysqli_query($conn, "SELECT * FROM products");
+    while ($row = mysqli_fetch_assoc($result)) {
+    ?> -->
+        <!-- <div class="card" style="width: 18rem;">
+            <img src="./admin/uploads/<?= $row['img']; ?>" class="card-img-top" alt="..." height="375">
+            <div class="card-body">
+                <h5 class="card-title"><?= $row['name']; ?></h5>
+                <p class="card-text">₹<?= $row["price"]; ?></p>
+                <p class="card-text"><?= $row["des"]; ?></p>
+                <a href="/stylish/src/product_details.php?id=<?= $row["id"]?>" class="btn btn-primary">Buy now</a>
+            </div>
+        </div> -->
+    <!-- <?php } ?> -->
+<!-- </div> -->
+<!-- <?php
+require("../components/footer.php");
+?> -->
